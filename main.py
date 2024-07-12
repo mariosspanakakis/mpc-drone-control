0e40d4913926ad72843b963d6ecb4ec88cf96655
Marios <m.spanakakis@tum.de>
2024-07-12 15:55:18 +0200

    reformatting

--- main.py
+++ main.py
@@ -37,8 +37,7 @@ initial_state = np.array([
 xy_des = np.array([initial_state[0], initial_state[2]])
 
 # nonlinear drone model
-drone = Drone(
-    constants=drone_constants, initial_state=copy(initial_state), Ts=Ts)
+drone = Drone(constants=drone_constants, initial_state=copy(initial_state), Ts=Ts)
 # linearization and discretization of the model to obtain the matrices Ad and Bd
 Ad, Bd = drone.linearize()
 
@@ -64,25 +63,17 @@ xmax = np.array([
 umin = np.array([0., 0.]) - drone.u_OP
 umax = np.array([1., 1.]) - drone.u_OP
 
-# parameter set obtained via Bayesian Optimization; does not perform too well for general trajectories
-"""opt_params = {'q_x': 65.6121719067622,
-              'q_y': 86.39821246377763,
-              'q_xy_dot': 2.18474740680037,
-              'q_theta': 32.17697421563099,
-              'q_theta_dot': 1713.808254107117,
-              'q_u': 4.386436533929654}"""
-
 # define weight matrices
 Qx = np.diag([params['qx'], params['qy'], params['qvx'], params['qvy'], params['qtheta'], params['qdtheta']])
 Qu = np.diag([params['qu'], params['qu']])
-#Qx = np.diag([opt_params['q_x'], opt_params['q_xy_dot'], opt_params['q_y'], opt_params['q_xy_dot'], opt_params['q_theta'], opt_params['q_theta_dot']])
-#Qu = np.diag([opt_params['q_u'], opt_params['q_u']])
 Qn = Qx
 
 # generate linear MPC controller
-K = LinearQuadraticMPC(Ad=Ad, Bd=Bd, N=N,
-                       xmin=xmin, xmax=xmax, umin=umin, umax=umax,
-                       Qx=Qx, Qu=Qu, Qn=Qn, info=False)
+K = LinearQuadraticMPC(
+    Ad=Ad, Bd=Bd, N=N,
+    xmin=xmin, xmax=xmax, umin=umin, umax=umax,
+    Qx=Qx, Qu=Qu, Qn=Qn, info=False
+)
 
 # trajectory planner
 planner = LinearTrajectoryPlanner(N=N, Ts=Ts, initial_state=initial_state)
@@ -108,9 +99,12 @@ plt.show()
 # assign user click coordinates as new goal for the drone
 def on_click(event):
     if event.inaxes:
-        planner.calculate_trajectory(P=np.array([drone.z[0], drone.z[2]]),
-                                     Q=np.array([event.xdata, event.ydata]),
-                                     v_max=4., a=1.)
+        planner.calculate_trajectory(
+            P=np.array([drone.z[0], drone.z[2]]),
+            Q=np.array([event.xdata, event.ydata]),
+            v_max=4.,
+            a=1.
+        )
     #K.update_weight_matrices(Qx=Qx, Qu=Qu, Qn=Qn)
 plt.connect('button_press_event', on_click)
 
@@ -119,8 +113,8 @@ def on_close(event):
     quit()
 fig.canvas.mpl_connect('close_event', on_close)
 
-i = 0
 # animation cycle
+#i = 0
 while True:
     # get current drone position
     xy_act = (drone.z[0], drone.z[2])
@@ -142,8 +136,8 @@ while True:
     # update the drone visualization
     move_drone(drone=drone_rect, x=drone.z[0], y=drone.z[2], theta=drone.z[4])
 
-    if i % 10 == 0:
-        visualize_drone(x=drone.z[0], y=drone.z[2], theta=drone.z[4], ax=ax, color='grey')
+    #if i % 10 == 0:
+    #    visualize_drone(x=drone.z[0], y=drone.z[2], theta=drone.z[4], ax=ax, color='grey')
 
     plt.pause(0.001)
-    i += 1
+    #i += 1
